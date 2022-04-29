71100514bded8e2eacddaa705a00ac63990153ad
Benjamin Efenudu <benjaminefenudu@gmail.com>
2022-04-29 15:23:48 +0100

    Demo Ready

--- src/interface/http/controllers/payment.controller.ts
+++ src/interface/http/controllers/payment.controller.ts
@@ -4,30 +4,41 @@ import PaymentRepository from '../../../infra/repository/payment.repository';
 
 class PaymentController {
   createPayment: CreatePayment;
+  paymentRepository: PaymentRepository;
 
-  constructor({ createPayment }: { createPayment: CreatePayment }) {
+  constructor({
+    createPayment,
+    paymentRepository
+  }: {
+    createPayment: CreatePayment;
+    paymentRepository: PaymentRepository;
+  }) {
     this.createPayment = createPayment;
+    this.paymentRepository = paymentRepository
   }
 
   async create(req: Request, res: Response) {
     try {
       const payload = req.body.order;
-      const payment = await this.createPayment.execute(payload);
+      const paymentSuccess = await this.createPayment.execute(payload);
 
-      if (!payment) {
-        return res.status(400).json({
+      if (!paymentSuccess) {
+        return res.status(200).json({
           success: false,
           msg: `Insufficient funds`,
+          paymentSuccess,
         });
       }
 
-      if (payment) {
-        return res.status(201).json({
-          success: true,
-          msg: `Payment successful`,
-          payment: payment,
-        });
-      }
+      payload.orderStatus = 'complete';
+      const payment = await this.paymentRepository.create(payload);
+
+      return res.status(200).json({
+        success: true,
+        msg: `Payment successful`,
+        paymentSuccess,
+        payment,
+      });
     } catch (error) {
       if (error instanceof Error) {
         res.status(400).json({ success: false, msg: `${error.message}` });
--- src/usecases/makePayment.ts
+++ src/usecases/makePayment.ts
@@ -1,21 +1,16 @@
 import axios from 'axios';
 import PaymentModel from '../infra/database/models/mongoose/payment.model';
-import PaymentRepository from '../infra/repository/payment.repository';
 import { PaymentDocument } from '../infra/database/models/mongoose/payment.model';
 
 class CreatePayment {
   paymentModel: typeof PaymentModel;
-  paymentRepository: PaymentRepository;
 
   constructor({
     paymentModel,
-    paymentRepository,
   }: {
     paymentModel: typeof PaymentModel;
-    paymentRepository: PaymentRepository;
   }) {
     this.paymentModel = paymentModel;
-    this.paymentRepository = paymentRepository;
   }
 
   async execute(payload: PaymentDocument) {
@@ -27,15 +22,7 @@ class CreatePayment {
           amount: payload.amount,
         })
         .then(async (response) => {
-          if (response.data.success) {
-            payload.orderStatus = 'complete';
-            const payment = await this.paymentRepository.create(payload);
-
-            return  payment;
-          } else {
-            console.log('Insufficient funds');
-            return;
-          }
+          return response.data.success
         })
         .catch((error) => {
           console.log('error: ', error?.message);
